31aeca815f66b8957ace3d548d4aa8fd187f19bf
Nahuel Alberto Cachambi <cachasan.10@gmail.com>
2023-05-21 12:44:21 -0300

    Se modifico el tipo de dato de la hora inicial ,final y el id.

--- Tp2_Grupo30/src/main/java/ar/edu/unju/fi/model/Servicio.java
+++ Tp2_Grupo30/src/main/java/ar/edu/unju/fi/model/Servicio.java
@@ -1,17 +1,25 @@
 package ar.edu.unju.fi.model;
 
+import java.time.LocalTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 public class Servicio {
-	private String id;
-	private String startTime;
-	private String finishTime;
+	private int id;
+	@DateTimeFormat(pattern = "hh:mm")
+	private LocalTime startTime;
+	@DateTimeFormat(pattern = "hh:mm")
+	private LocalTime finishTime;
 	private String fullName;
 	private String day;
-	
+
 	public Servicio() {
-		
+
 	}
-	public Servicio(String id, String startTime, String finishTime, String fullName, String day) {
+
+	
+	public Servicio(int id, LocalTime startTime, LocalTime finishTime, String fullName, String day) {
+		super();
 		this.id = id;
 		this.startTime = startTime;
 		this.finishTime = finishTime;
@@ -20,45 +28,49 @@ public class Servicio {
 	}
 
 
-	public String getStartTime() {
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+	public LocalTime getStartTime() {
 		return startTime;
 	}
 
 
-	public void setStartTime(String startTime) {
+	public void setStartTime(LocalTime startTime) {
 		this.startTime = startTime;
 	}
 
 
-	public String getFinishTime() {
+	public LocalTime getFinishTime() {
 		return finishTime;
 	}
 
 
-	public void setFinishTime(String finishTime) {
+	public void setFinishTime(LocalTime finishTime) {
 		this.finishTime = finishTime;
 	}
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	
+
+
 	public String getFullName() {
 		return fullName;
 	}
+
 	public void setFullName(String fullName) {
 		this.fullName = fullName;
 	}
+
 	public String getDay() {
 		return day;
 	}
+
 	public void setDay(String day) {
 		this.day = day;
 	}
-	
-	
+
 }
