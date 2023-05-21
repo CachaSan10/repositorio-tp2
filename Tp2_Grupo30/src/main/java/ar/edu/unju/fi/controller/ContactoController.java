e9b13d5995a80fabe7dfb75ee9ebfecf85f154cc
Nahuel Alberto Cachambi <cachasan.10@gmail.com>
2023-05-21 00:21:50 -0300

    Se elimino librerias que no son utilizadas y se actualizo la version del proyecto.

--- Tp2_Grupo30/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
+++ Tp2_Grupo30/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
@@ -3,7 +3,6 @@ package ar.edu.unju.fi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import ch.qos.logback.core.model.Model;
 
 /**
 *Villena Franco
