ad242ca516646b5aaf4962c23682f8db27ee7ac3
Nahuel Alberto Cachambi <cachasan.10@gmail.com>
2023-06-06 16:33:01 -0300

    Responsable: Nahuel Alberto Cachambi user:CachaSan10 Descripción: Creacion de la interface de service consejo. Close #108

--- Tp2_Grupo30/src/main/java/ar/edu/unju/fi/service/IConsejoService.java
+++ Tp2_Grupo30/src/main/java/ar/edu/unju/fi/service/IConsejoService.java
@@ -1,5 +1,20 @@
 package ar.edu.unju.fi.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import ar.edu.unju.fi.listas.ListaConsejo;
+import ar.edu.unju.fi.model.Consejo;
+
 public interface IConsejoService {
 
+	public ListaConsejo getConsejos();
+	
+	public void addConsejo(Consejo consejo,MultipartFile imagen);
+	
+	public Consejo getConsejo(int id);
+	
+	public void updateConsejo(Consejo consejoModificado,MultipartFile imagen);
+	
+	public void deleteConsejo(int id);
+	
 }
