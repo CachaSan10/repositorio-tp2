ea9fc544825693959cbbf43ad60f0847849ac319
Abel Nestor Peñaloza <abelnrpenaloza@gmail.com>
2023-06-10 17:41:09 -0300

    Agregar comentarios en servicios y modificar nuevoservicio.html
    user:AbelNPenhaloza
    Descripción: Agregar comentarios y modificar nuevoservicio.html
    Close #119

--- Tp2_Grupo30/src/main/java/ar/edu/unju/fi/controller/ServicioController.java
+++ Tp2_Grupo30/src/main/java/ar/edu/unju/fi/controller/ServicioController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import ar.edu.unju.fi.listas.ListaServicio;
 import ar.edu.unju.fi.model.Servicio;
 import ar.edu.unju.fi.service.IServicioService;
 import jakarta.validation.Valid;
