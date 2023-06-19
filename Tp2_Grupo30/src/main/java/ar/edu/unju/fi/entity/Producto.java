fc3f495f7f5d120fceb959310e73cc23867a30a7
Nahuel Alberto Cachambi <cachasan.10@gmail.com>
2023-06-19 13:13:12 -0300

    Responsable: Nahuel Alberto Cachambi user: CachaSan10 Descripción: Creacion de la rama secundaria producto-repository

--- Tp2_Grupo30/src/main/java/ar/edu/unju/fi/entity/Producto.java
+++ Tp2_Grupo30/src/main/java/ar/edu/unju/fi/entity/Producto.java
@@ -2,6 +2,7 @@ package ar.edu.unju.fi.entity;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
@@ -13,6 +14,7 @@ import jakarta.validation.constraints.Size;
  * @author Acosta Jose David 
  */
 @Component
+@Entity
 public class Producto {
 	private int id;
 	@NotEmpty(message="El nombre del producto no puede estar vacio")
